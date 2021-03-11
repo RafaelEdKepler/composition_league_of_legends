de646be2ead142c1a103f809bc5b230f3221e3e7
Rafael Eduardo Kepler <rafael.kepler@hotmail.com>
2021-03-11 19:39:26 -0300

    Ajustando efeito que simulara o campeao a ser selecionado

--- src/components/ChampionBox/index.tsx
+++ src/components/ChampionBox/index.tsx
@@ -1,8 +1,30 @@
+import { useState, useEffect } from 'react';
+
 import { Container } from './style'
 
 export default function ChampionBox() {
+    const [bright, setBright] = useState(100);
+    const [isGrowing, setIsGrowing] = useState(false);
+
+    useEffect(() => {
+        setTimeout(function () {
+            if (bright === 130) {
+                setIsGrowing(false);
+            }
+            if (bright === 100) {
+                setIsGrowing(true);
+            }
+
+            if (!isGrowing) {
+                setBright(bright - 10);
+            } else {
+                setBright(bright + 10);
+            }
+        }, 200)
+    }, [bright]);
+
     return (
-        <Container>
+        <Container bright={bright}>
         </Container>
     );
 };
--- src/components/ChampionBox/style.ts
+++ src/components/ChampionBox/style.ts
@@ -10,5 +10,7 @@ export const Container = styled.div`
     border-color: #FF1;
     background: url("https://img.redbull.com/images/c_crop,x_0,y_0,h_405,w_1215/c_fill,w_1680,h_520/q_auto,f_auto/redbullcom/2019/08/22/ef08fb89-2622-4dcf-be1a-72b3352861fe/kaisa-league-of-legends");
     background-size: cover;
+    transition: all 0.2s ease;
+    filter: brightness(${props => props.bright}%);
 
 `;
